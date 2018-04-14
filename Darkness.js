9497af3ec3d081ed03d1609b2b940b4e1fa6fd42
Diskuid <38334440+Diskuid@users.noreply.github.com>
2018-04-13 21:14:39 -0500

    Update Darkness.js

--- Darkness.js
+++ Darkness.js
@@ -46,8 +46,6 @@ client.on("message", (message) => {
         
         message.channel.send({embed});
         } else
-
-                  } else
        if (msg.startsWith(prefix +"alola" )){
         const embed = new Discord.RichEmbed() 
         .setAuthor("Alola! "+message.author.username+" o/", message.author.avatarURL)
@@ -55,9 +53,7 @@ client.on("message", (message) => {
         .setImage("https://cdn.discordapp.com/attachments/434155755357208587/434535939147169797/alola_vulpix_7u7.gif");
         
         message.channel.send({embed});
-        } else
-           
-           
+        } else       
 if (msg.startsWith(prefix +"jirachi" )){
   const embed = new Discord.RichEmbed() 
   .setColor(0xd53939)
