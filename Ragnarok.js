89eaa1e20058fe781a12cfd49e7f362ec700b76e
Diskuid <38334440+Diskuid@users.noreply.github.com>
2018-06-08 12:03:20 -0500

    Update Ragnarok.js

--- Ragnarok.js
+++ Ragnarok.js
@@ -111,7 +111,22 @@ if (msg.startsWith(prefix +"darkrai" )){
     .addField("Campo en línea 3", "Puede tener un máximo de 25 campos.", true);
     
     message.channel.send({embed});
-    }
+    } else
+  if (msg.startsWith(prefix +"purge" )){
+    // This command removes all messages from all users in the channel, up to 100.
+    
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
+    
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
 });
 
 // THIS MUST BE THIS WAY
