df40c3f83aa2515cd339a52455e0c74c2052948c
Diskuid <38334440+Diskuid@users.noreply.github.com>
2018-06-09 10:58:37 -0500

    Update Ragnarok.js

--- Ragnarok.js
+++ Ragnarok.js
@@ -15,31 +15,41 @@ client.on("message", (message) => {
     let msg = message.content.toLowerCase();
     let cont = message.content.slice(prefix.length).split(" ");
     let args = cont.slice(1);
-
-    if (msg.startsWith(prefix + 'purge')) {
+    let sender = message.author;   
+   
+    // Purge
+    if (msg.startsWith(prefix + 'purge')) { // This time we have to use startsWith, since we will be adding a number to the end of the command.
+        // We have to wrap this in an async since awaits only work in them.
         async function purge() {
-            message.delete();
+            message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete.
 
-            if (!message.member.roles.find("name", "bot-commander")) {
-                message.channel.send('you need the bot-commander role to use this command.')
-                return;
+            // Now, we want to check if the user has the `bot-commander` role, you can change this to whatever you want.
+            if (!message.member.roles.find("name", "bot-commander")) { // This checks to see if they DONT have it, the "!" inverts the true/false
+                message.channel.send('You need the \`bot-commander\` role to use this command.'); // This tells the user in chat that they need the role.
+                return; // this returns the code, so the rest doesn't run.
             }
 
+            // We want to check if the argument is a number
             if (isNaN(args[0])) {
-                message.channel.send('Please use a number');
+                // Sends a message to the channel.
+                message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'purge <amount>'); //\n means new line.
+                // Cancels out of the script, so the rest doesn't run.
                 return;
             }
 
-            const fetched = await message.channel.fetchMessages({limit: args[0]});
-            console.log(fetched.size + 'messages found, deleting...');
+            const fetched = await message.channel.fetchMessages({limit: args[0]}); // This grabs the last number(args) of messages in the channel.
+            console.log(fetched.size + ' messages found, deleting...'); // Lets post into console how many messages we are deleting
 
+            // Deleting the messages
             message.channel.bulkDelete(fetched)
-            .catch(error => message.channel.send('Error: ${error}'));
+                .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
+
         }
 
-     purge();
-   
-  } 
+        // We want to make sure we call the function whenever the purge command is run.
+        purge(); // Make sure this is inside the if(msg.startsWith)
+
+    }
 if (msg.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
   } else
