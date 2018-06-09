e88ee9a4685a6416080b846a6edea0d616700a76
Diskuid <38334440+Diskuid@users.noreply.github.com>
2018-06-09 12:06:45 -0500

    Update Ragnarok.js

--- Ragnarok.js
+++ Ragnarok.js
@@ -52,6 +52,19 @@ client.on("message", (message) => {
         purge(); // Make sure this is inside the if(msg.startsWith)
 
     }
+if(command === 'kick' ){
+
+    let user = message.mentions.users.first();
+    let razon = args.slice(1).join(' ');
+    
+    if (message.mentions.users.size < 1) return message.reply('Debe mencionar a alguien.').catch(console.error);
+    if (!razon) return message.channel.send('Escriba una razón, `-kick @username [razón]`');
+    if (!message.guild.member(user).kickable) return message.reply('No puedo patear al usuario mencionado.');
+     
+    message.guild.member(user).kick(razon);
+    message.channel.send(`**${user.username}**, fue pateado del servidor, razón: ${razon}.`);
+
+}
 if (msg.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
   } else
